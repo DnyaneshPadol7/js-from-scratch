425f2ae569dc19e5b7f093e5810419b753a2da05
Dnyanesh Padol <dpadol14@gmail.com>
2026-08-16 03:29:30 +0530

    Initial commit

---
+++ Lecture_29/index.js
@@ -0,0 +1,44 @@
+// console.log("Hello Jarvis");
+
+// // What is variables in JS
+// age = 23;
+// // name = "Dnyanesh";
+// console.log(age);
+
+// // Use of var , let and const
+
+// // use of var keyword
+// it allows only updation
+// var age = 24;
+// console.log(age);
+// var age = 25;  // using var keyword we can redeclare the variable
+
+// console.log(age);
+
+
+// // use of let keyword 
+// let name = "Dnyanesh";
+// console.log(name);
+
+// // let name = "Vaibhavi"; // it never allows the reinitlization/re-declaration of same variables 
+// console.log(name);
+
+
+// use of const 
+// const never allows redeclare/redeclaration or updataion 
+const adharcard = 8879887987899;
+console.log(adharcard);
+
+// const adharcard = 882309898989; it will gives us erorr 
+
+const userName = "Dnyanu";
+const greettingMessage = `Hii ${console.log("nested console.log")}`  // nested console.log
+console.log(greettingMessage);
+
+// let user;
+// console.log(user);  ex. of undefined
+
+let products = null;
+console.log(products);
+console.log(typeof products);  // example of null
+
