2730bd255ccc68c098fd0e3d200d7502c8429cb4
Dnyanesh Padol <dpadol14@gmail.com>
2026-09-08 15:12:36 +0530

    updated Js_assignment

---
+++ Js_Assignment/Programming_Fundamentals.js
@@ -0,0 +1,34 @@
+console.log("-------------------------1. Basic Calculator--------------------------------");
+var num1 = 44;
+var num2 = 11;
+console.log("Number 1 is "+num1);
+console.log("Number 2 is "+num2);
+console.log(" ");
+
+add = num1+num2
+console.log("Addition = "+add);
+sub = num1-num2;
+console.log("Subtraction = "+sub);
+console.log("Multiplication = "+num1*num2);
+console.log("Division = "+num1/num2);
+console.log("Remainder = "+num1%num2);
+console.log("----------------------------------------------------------------------------");
+
+
+
+console.log("-------------------------2. Swap Two Numbers--------------------------------");
+console.log("Before Swapping: ");
+var a = 10;
+var b = 20;
+console.log("Value of a = "+a);
+console.log("Value of b = "+b);
+console.log(" ");
+
+console.log("After Swapping: ");
+var c;
+c = a;
+a = b;
+b = c;
+console.log("Value of a = "+a);
+console.log("Value of b = "+b);
+
